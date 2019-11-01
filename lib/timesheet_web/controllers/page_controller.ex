349006b5878ceb5e6773f462bc20229cd96fef69
sikang hu <sikang_hu@berkeley.edu>
2019-11-01 18:48:00 -0400

    123

--- lib/timesheet_web/controllers/page_controller.ex
+++ lib/timesheet_web/controllers/page_controller.ex
@@ -5,12 +5,7 @@ defmodule TimesheetWeb.PageController do
   	user = conn.assigns[:current_user]
 
   	if user do
-  		if user.manager_id do
-  			redirect(conn, to: Routes.sheet_path(conn, :index))
-  		else
-  			redirect(conn, to: Routes.user_path(conn, :index))
-  		end
-  		
+  		redirect(conn, to: Routes.sheet_path(conn, :index))
   	else 
   		render(conn, "index.html")
   	end
