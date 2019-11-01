07620e08150d9768eb5eee04deab1605ecfe60b3
sikang hu <sikang_hu@berkeley.edu>
2019-11-01 15:47:07 -0400

    modified schema

--- priv/repo/migrations/20191031033752_create_users.exs
+++ priv/repo/migrations/20191031033752_create_users.exs
@@ -3,9 +3,9 @@ defmodule Timesheet.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :email, :string
-      add :name, :string
-      add :manager_id, :integer, default: -1
+      add :email, :string, null: false
+      add :name, :string, null: false
+      add :manager_id, :integer, references(:users), default: -1
 
       timestamps()
     end
